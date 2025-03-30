07ad3cec26ec422799fe1acca29267a9f710a01a
nayankamalverma <nayankamalverma@gmail.com>
2025-03-30 12:17:34 +0530

    .

--- Assets/Scripts/Enemy/EnemyObjectPool.cs
+++ Assets/Scripts/Enemy/EnemyObjectPool.cs
@@ -4,72 +4,73 @@ using UnityEngine;
 
 namespace Assets.Scripts.Enemy
 {
-	public class EnemyObjectPool 
-	{
-		private Transform enemyParent;
-		private EnemyService enemyService;
+    public class EnemyObjectPool
+    {
+        private Transform enemyParent;
+        private EnemyService enemyService;
         public List<PooledItem> pooledItems = new List<PooledItem>();
 
         public EnemyObjectPool(EnemyService enemyService, Transform enemyParent)
-		{
+        {
             this.enemyService = enemyService;
-			this.enemyParent = enemyParent;
+            this.enemyParent = enemyParent;
         }
 
-		public EnemyController GetEnemy(EnemyType enemyType)
-		{
-			if (pooledItems.Count > 0)
-			{
-				PooledItem item = pooledItems.Find(i =>i.enemy.GetEnemyType()==enemyType && !i.isUsed);
-				if (item != null)
-				{
-					item.isUsed = true;
-					item.enemy.gameObject.SetActive(true);
-					item.enemy.enabled = true;
-					return item.enemy;
-				}
-			}
-			return CreateNewPooledItem(enemyType);
-		}
+        public EnemyController GetEnemy(EnemyType enemyType)
+        {
+            if (pooledItems.Count > 0)
+            {
+                PooledItem item = pooledItems.Find(i => i.enemy.GetEnemyType() == enemyType && !i.isUsed);
+                if (item != null)
+                {
+                    item.isUsed = true;
+                    item.enemy.gameObject.SetActive(true);
+                    item.enemy.enabled = true;
+                    return item.enemy;
+                }
+            }
 
-		private EnemyController CreateNewPooledItem(EnemyType enemyType)
-		{
-			PooledItem item = new PooledItem();
-			item.enemy = CreateItem(enemyType);
+            return CreateNewPooledItem(enemyType);
+        }
+
+        private EnemyController CreateNewPooledItem(EnemyType enemyType)
+        {
+            PooledItem item = new PooledItem();
+            item.enemy = CreateItem(enemyType);
             item.isUsed = true;
-			pooledItems.Add(item);
-			return item.enemy;
-		}
+            pooledItems.Add(item);
+            return item.enemy;
+        }
 
-		protected  EnemyController CreateItem(EnemyType enemyType)
-		{
-			EnemyScriptableObjects enemySO= enemyService.enemyList.Find(i => i.enemyType == enemyType);
+        protected EnemyController CreateItem(EnemyType enemyType)
+        {
+            EnemyScriptableObjects enemySO = enemyService.enemyList.Find(i => i.enemyType == enemyType);
             EnemyController enemy = Object.Instantiate<EnemyController>(enemySO.enemy, enemyParent);
-			enemy.SetReferences(enemyService, enemySO, enemyService.playerTransform);
-			return enemy;
+            enemy.SetReferences(enemyService, enemySO, enemyService.playerTransform);
+            return enemy;
         }
 
 
-		public virtual void ReturnItem(EnemyController item)
-		{	
-			PooledItem pooledItem = pooledItems.Find(i => i.enemy == item);
-			pooledItem.enemy.gameObject.SetActive(false);
+        public virtual void ReturnItem(EnemyController item)
+        {
+            PooledItem pooledItem = pooledItems.Find(i => i.enemy == item);
+            pooledItem.enemy.gameObject.SetActive(false);
             pooledItem.isUsed = false;
         }
 
-		public void ReturnAllItem()
-		{
-			foreach (var item in pooledItems)
-			{
-				item.enemy.gameObject.SetActive(false);
-				item.isUsed = false;
-			}
-		}
+        public void ReturnAllItem()
+        {
+            foreach (var item in pooledItems)
+            {
+                item.enemy.gameObject.SetActive(false);
+                item.isUsed = false;
+            }
+        }
 
-		public class PooledItem
-		{
-			public EnemyController enemy;
-			public bool isUsed;
-		}
-	}
+        public class PooledItem
+        {
+            public EnemyController enemy;
+            public bool isUsed;
+        }
+    }
 }
